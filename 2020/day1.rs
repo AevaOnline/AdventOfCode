b13a5b68b9f494fdd797a9df138cfc641b2d099c
Aeva Black <806320+AevaOnline@users.noreply.github.com>
2020-12-01 08:42:17 +0000

    add a few comments

--- 2020/day1.rs
+++ 2020/day1.rs
@@ -1,13 +1,18 @@
 fn main() {
+    // define constants for the game input
     let expenses = [1721, 979, 366, 299, 675, 1456];
     let target = 2020;
 
+    // our index pointers need to be scoped outside the loops
+    // so we can access them after!
     let (mut i, mut j)  = (0, 0);
 
+    // set a label so we can easily break out when we find the solution
     'outer: while i < expenses.len() {
         j = i + 1;
         while j < expenses.len()  {
             let x = expenses[i] + expenses[j];
+            // check if this is the record combo we want
             if x == target {
                 break 'outer;
             }
@@ -15,5 +20,7 @@ fn main() {
         }
         i += 1;
     }
+
+    // dont forget to multiply the values!
     println!("{}",  expenses[i] * expenses[j] );
 }
